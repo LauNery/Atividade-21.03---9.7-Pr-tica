463551c22ecda4a0c665cb3d736a0d3c8957f9d0
Laura Nery <laura.olivernery@gmail.com>
2025-03-25 13:44:01 -0300

    Finalizando Lista1_Exercício1

--- src/atividadePratica9_7/Lista1_Exercício2.java
+++ src/atividadePratica9_7/Lista1_Exercício2.java
@@ -8,31 +8,38 @@ public class Lista1_Exercício2 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int vetorInteiros[] = {1}; 
-		{
-			
+		int vetorInteiros[] = new int[10];  
+		int soma = 0;
+		
+		System.out.println("Digite os elementos: ");
 		for(int i = 0; i < vetorInteiros.length; i++) {
-			System.out.println("Digite os elementos nos índices ímpares: ");
 		vetorInteiros[i] = scanner.nextInt();
 		}
 				
-	  for(int i = 0; i < vetorInteiros.length; i++) {
-			System.out.println("Digite os elementos nos índices pares:: ");
-		   vetorInteiros[i] = scanner.nextInt();
+	    System.out.println("Elementos dos índices ímpares: ");
+		for(int i = 1; i < vetorInteiros.length ; i += 2) {
+			System.out.print(vetorInteiros[i] + " ");
 	  }
 				
-	  for(int i = 0; i < vetorInteiros.length; i++) {
-			System.out.println("Digite é a soma: ");
-	       vetorInteiros[i] = scanner.nextInt();
+		System.out.println("Elementos pares");
+		for(int i = 0; i < vetorInteiros.length; i++) {
+			if(vetorInteiros[i] % 2 == 0) {
+				System.out.println(vetorInteiros[i] + " ");
+			}
 	  }
-				
-			 Arrays.sort(vetorInteiros); {
-			System.out.println("Digite é a média: 5.50: ");
+			for(int i = 0; i < vetorInteiros.length ; i++) {
+			soma += vetorInteiros[i];
 			
-		}
+		} 
+			System.out.println("Soma: " + soma);
+			float media = (float) soma / vetorInteiros.length;
+			
+			System.out.printf("Média: %.2f " , media);
+			
 	}
 }
-}
+
+
 
 
 
